f526905b4743c7f90d6a15d0f04b83f351660570
Salvador Montero <salvadormdev@gmail.com>
2024-08-14 11:03:56 -0400

    agregando animaciones al proyecyo con scrollreveal

---
+++ src/js/index.js
@@ -0,0 +1,326 @@
+window.sr = ScrollReveal();
+//   sr.reveal('.header ul .li1', {
+//     duration: 2500,
+//     origin: 'bottom',
+//     distance: '-50px'
+//   });
+
+//   sr.reveal('.header ul .li2', {
+//     duration: 2750,
+//     origin: 'bottom',
+//     distance: '-75px'
+//   });
+
+//   sr.reveal('.header ul .li3', {
+//     duration: 3000,
+//     origin: 'bottom',
+//     distance: '-100px'
+//   });
+
+//   sr.reveal('.header ul .li4', {
+//     duration: 3250,
+//     origin: 'bottom',
+//     distance: '-125px'
+//   });
+
+//   sr.reveal('.header ul .li5', {
+//     duration: 3500,
+//     origin: 'bottom',
+//     distance: '-150px'
+//   });
+
+//   sr.reveal('.header ul .li6', {
+//     duration: 4000,
+//     origin: 'bottom',
+//     distance: '-175px'
+//   });
+
+  sr.reveal('.header .container1', {
+    duration: 1500,
+    origin: 'bottom',
+    distance: '-100px'
+  });
+
+  sr.reveal('.about-animation', {
+    duration: 3500,
+    origin: 'top',
+    distance: '-300px'
+  });
+
+  // sr.reveal('.about-animation .informacoes .titulos', {
+  //   duration: 4000,
+  //   origin: 'left',
+  //   distance: '-300px'
+  // });
+
+  sr.reveal('.hero .banner1', {
+    duration: 1000,
+    origin: 'rigth',
+    distance: '-350px'
+  });
+
+  sr.reveal('.hero .banner4', {
+    duration: 3000,
+    origin: 'top',
+    distance: '-350px'
+  });
+
+  sr.reveal('.hero .banner5', {
+    duration: 3500,
+    origin: 'top',
+    distance: '-400px'
+  });
+
+  sr.reveal('.beneficios', {
+    duration: 4000,
+    origin: 'rigth',
+    distance: '-300px'
+  });
+
+  sr.reveal('.categories .item1', {
+    duration: 3500,
+    origin: 'rigth',
+    distance: '-300px'
+  });
+  
+  sr.reveal('.categories .item2', {
+    duration: 3750,
+    origin: 'rigth',
+    distance: '-350px'
+  });
+
+  sr.reveal('.categories .item3', {
+    duration: 4000,
+    origin: 'rigth',
+    distance: '-400px'
+  });
+
+  sr.reveal('.categories .item4', {
+    duration: 3500,
+    origin: 'rigth',
+    distance: '-300px'
+  });
+  
+  sr.reveal('.categories .item5', {
+    duration: 3750,
+    origin: 'rigth',
+    distance: '-350px'
+  });
+
+  sr.reveal('.categories .item6', {
+    duration: 4000,
+    origin: 'rigth',
+    distance: '-400px'
+  });
+
+  sr.reveal('.categories .item7', {
+    duration: 3500,
+    origin: 'rigth',
+    distance: '-300px'
+  });
+  
+  sr.reveal('.categories .item8', {
+    duration: 3750,
+    origin: 'rigth',
+    distance: '-350px'
+  });
+
+  sr.reveal('.categories .item9', {
+    duration: 4000,
+    origin: 'rigth',
+    distance: '-400px'
+  });
+  
+  sr.reveal('.categories .item10', {
+    duration: 3500,
+    origin: 'rigth',
+    distance: '-300px'
+  });
+  
+  sr.reveal('.categories .item11', {
+    duration: 3750,
+    origin: 'rigth',
+    distance: '-350px'
+  });
+
+  sr.reveal('.categories .item12', {
+    duration: 4000,
+    origin: 'rigth',
+    distance: '-400px'
+  });
+
+
+  sr.reveal('.products .product-1', {
+    duration: 3500,
+    origin: 'rigth',
+    distance: '-300px'
+  });
+
+  sr.reveal('.products .product-2', {
+    duration: 3750,
+    origin: 'rigth',
+    distance: '-300px'
+  });
+
+  sr.reveal('.products .product-3', {
+    duration: 4000,
+    origin: 'rigth',
+    distance: '-300px'
+  });
+
+  sr.reveal('.products .product-4', {
+    duration: 4500,
+    origin: 'rigth',
+    distance: '-300px'
+  });
+
+  sr.reveal('.products .product-5', {
+    duration: 3500,
+    origin: 'rigth',
+    distance: '-300px'
+  });
+
+  sr.reveal('.products .product-6', {
+    duration: 3750,
+    origin: 'rigth',
+    distance: '-300px'
+  });
+
+  sr.reveal('.products .product-7', {
+    duration: 4000,
+    origin: 'rigth',
+    distance: '-300px'
+  });
+
+  sr.reveal('.products .product-8', {
+    duration: 4500,
+    origin: 'rigth',
+    distance: '-300px'
+  });
+
+  sr.reveal('.products2 .product-1', {
+    duration: 3500,
+    origin: 'rigth',
+    distance: '-300px'
+  });
+
+  sr.reveal('.products2 .product-2', {
+    duration: 3750,
+    origin: 'rigth',
+    distance: '-300px'
+  });
+
+  sr.reveal('.products2 .product-3', {
+    duration: 4000,
+    origin: 'rigth',
+    distance: '-300px'
+  });
+
+  sr.reveal('.products2 .product-4', {
+    duration: 4500,
+    origin: 'rigth',
+    distance: '-300px'
+  });
+
+  sr.reveal('.products2 .product-5', {
+    duration: 3500,
+    origin: 'rigth',
+    distance: '-300px'
+  });
+
+  sr.reveal('.products2 .product-6', {
+    duration: 3750,
+    origin: 'rigth',
+    distance: '-300px'
+  });
+
+  sr.reveal('.products2 .product-7', {
+    duration: 4000,
+    origin: 'rigth',
+    distance: '-300px'
+  });
+
+  sr.reveal('.products2 .product-8', {
+    duration: 4500,
+    origin: 'rigth',
+    distance: '-300px'
+  });
+  
+  
+  sr.reveal('.card-1', {
+    duration: 3000,
+    origin: 'top',
+    distance: '-300px'
+  });
+
+  sr.reveal('.card-2', {
+    duration: 3000,
+    origin: 'top',
+    distance: '-300px'
+  });
+
+  sr.reveal('.card-3', {
+    duration: 3500,
+    origin: 'top',
+    distance: '-300px'
+  });
+
+  sr.reveal('.card-4', {
+    duration: 3500,
+    origin: 'top',
+    distance: '-300px'
+  });
+
+  sr.reveal('.card-5', {
+    duration: 4000,
+    origin: 'top',
+    distance: '-300px'
+  });
+
+  sr.reveal('.card-6', {
+    duration: 4000,
+    origin: 'top',
+    distance: '-300px'
+  });
+
+  sr.reveal('.products .titulo', {
+    duration: 4000,
+    origin: 'rigth',
+    distance: '-300px'
+  });
+
+  sr.reveal('.oferta .bg', {
+    duration: 4000,
+    origin: 'top',
+    distance: '-300px'
+  });
+
+  sr.reveal('.team .titulo', {
+    duration: 4000,
+    origin: 'rigth',
+    distance: '-300px'
+  });
+
+  sr.reveal('.team-tablet .titulo', {
+    duration: 4000,
+    origin: 'rigth',
+    distance: '-300px'
+  });
+
+  sr.reveal('.team-mobile .titulo', {
+    duration: 4000,
+    origin: 'rigth',
+    distance: '-300px'
+  });
+
+  sr.reveal('.testimonials .testimonio-1', {
+    duration: 2000,
+    origin: 'top',
+    distance: '-300px'
+  });
+
+  sr.reveal('.testimonials .testimonio-2', {
+    duration: 2500,
+    origin: 'top',
+    distance: '-300px'
+  });
